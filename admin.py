9909fbd30e2e8be3a5060c31d232d8be0800db2a
Akash Jyoti Sahoo <akashvana2001@gmail.com>
2022-09-01 12:51:15 +0530

    cleaned up code

--- admin.py
+++ admin.py
@@ -8,17 +8,19 @@ from os import path
 print("This is the admin page that will verify the user and can view the message sent by the user")
 
 ADMIN_PRIVATE_KEY = "0x8c84d32cc09a04f8b3d9d8c766f647267bf59b9691c693d45cfad80c0e60808c"
-# first load the messages json
+
+# admin loads all the messages that have been sent so far
 file_name = "messages.json"
 
 with open(file_name, "r") as msg: 
     messages = json.load(msg)
 
+# admin loads all the users that have been registered so far to get their details 
 user_file = "users.json"
 with open(user_file, "r") as usr: 
     users = json.load(usr)
 
-# compare the users json and get the verifying key for the user. First decrypt the message using admin private key and then verify using verifying key 
+# compare the users json and get the verifying key for the user. First decrypt the message using admin private key and then verify using verifying key of the user
 for msg in messages: 
     user_name = msg["user_name"]
     user_id = msg["user_id"]
